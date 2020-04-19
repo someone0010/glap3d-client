048925048510ff0fc76a5e62aa8f9a0e8c935f34
Goblin011 <44930310+someone0010@users.noreply.github.com>
2020-04-19 11:20:21 +0200

    First test

---
+++ assets/game.js
@@ -0,0 +1,276 @@
+var logger = document.getElementById("game-log");
+var stats = document.getElementById("stats");
+var serverUrl = "ws://127.0.0.1:3000";
+
+function log(text) {
+    if (logger.innerHTML.endsWith(text + "</div>")) {
+        logger.lastChild.textContent = logger.lastChild.textContent + " (x2)"
+    } else {
+        if (logger.innerHTML.endsWith(text + " (x2)</div>")) {
+            logger.lastChild.textContent = logger.lastChild.textContent.replace(" (x2)", "") + " (x3)"
+        } else {
+            if (logger.innerHTML.endsWith(text + " (x3)</div>")) {
+                logger.lastChild.textContent = logger.lastChild.textContent.replace(" (x3)", "") + " (x4)"
+            } else {
+                if (logger.innerHTML.endsWith(text + " (x4)</div>")) {
+                    logger.lastChild.textContent = logger.lastChild.textContent.replace(" (x4)", "") + " (x5)"
+                } else {
+                    if (logger.innerHTML.endsWith(text + " (x5)</div>")) {
+                        logger.lastChild.textContent = logger.lastChild.textContent.replace(" (x5)", "") + " (x6)"
+                    } else {
+                        if (logger.innerHTML.endsWith(text + " (x6)</div>")) {
+                            logger.lastChild.textContent = logger.lastChild.textContent.replace(" (x6)", "") + " (x7)"
+                        } else {
+                            if (logger.innerHTML.endsWith(text + " (x7)</div>")) {
+                                logger.lastChild.textContent = logger.lastChild.textContent.replace(" (x7)", "") + " (x8)"
+                            } else {
+                                if (logger.innerHTML.endsWith(text + " (x8)</div>")) {
+                                    logger.lastChild.textContent = logger.lastChild.textContent.replace(" (x8)", "") + " (x9)"
+                                } else {
+                                    if (logger.innerHTML.endsWith(text + " (x9)</div>")) {
+                                        logger.lastChild.textContent = logger.lastChild.textContent.replace(" (x9)", "") + " (x10)"
+                                    } else {
+                                        if (logger.children.length > 20) logger.firstChild.remove();
+
+                                        var div = document.createElement("div");
+                                        div.innerHTML = text;
+                                        div.setAttribute("class", "gameLogText");
+                                        logger.appendChild(div);
+                                        setTimeout(() => {
+                                            div.parentNode.removeChild(div);
+                                        }, 10000);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+}
+class player {
+    dataPool = 0
+
+    constructor() {
+
+    }
+    maps = [{
+        "name": "Test",
+        "version": "0.0.1a",
+        "id": 0,
+        "objects": [{
+            "g": 0,
+            "q": [0, 0, 0, 1],
+            "p": [0, -20, 0],
+            "s": [10, 10, 10],
+            "t": {
+                "dif": null,
+                "nor": null,
+                "ao": null,
+                "col": 0x00ee00,
+                "emi": 0x333333,
+                "emim": null,
+                "spc": null
+            }
+        }]
+    }]
+    drawtext3d(text2d, material, cssStyle) {
+        var canvas = document.createElement('canvas'),
+            ctx = canvas.getContext('2d');
+        ctx.font = cssStyle;
+        var textmeasure = ctx.measureText(text2d);
+        canvas.height = parseInt(ctx.font.match(/\d+/), 10);
+        canvas.width = textmeasure.width;
+        ctx.fillText(text2d, 0, 0);
+        var texture = new THREE.Texture(canvas);
+        material.map = texture;
+        var geometry = new THREE.PlaneBufferGeometry(canvas.width, canvas.height);
+        var mesh = new THREE.Mesh(geometry, material);
+        return mesh;
+    }
+
+    init() {
+        var canvas = document.createElement("canvas");
+        if (!(canvas.getContext("webgl") && window.WebGLRenderingContext)) {
+            if (window.WebGLRenderingContext) {
+                log("graphics card doesn't support webgl")
+            } else {
+                log("web browser doesn't support webgl")
+            }
+            return;
+        }
+
+        var scene = new THREE.Scene();
+        var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100000);
+        setInterval(this.everysecond, 1000);
+        var renderer = new THREE.WebGLRenderer({
+            logarithmicZBuffer: true,
+            antialias: true
+        });
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        document.body.appendChild(renderer.domElement)
+
+        function onWindowResize() {
+
+            camera.aspect = window.innerWidth / window.innerHeight;
+            camera.updateProjectionMatrix();
+
+            renderer.setSize(window.innerWidth, window.innerHeight);
+
+        }
+        window.addEventListener('resize', onWindowResize, false);
+        var ws = new WebSocket(serverUrl);
+        window.warws = ws;
+        var qualitySetting = "medium";
+        ws.onerror = function (err) {
+            log("Connection error: " + err);
+            log("Current WS state: " + ws.readyState)
+        }
+        ws.onclose = function (e) {
+            log("Disconnected: " + e.code);
+        }
+
+        var planets = [];
+        var sunG = new THREE.SphereBufferGeometry(10000, 64, 64);
+        var mercG = new THREE.SphereBufferGeometry(100, 64, 64);
+        var vensG = new THREE.SphereBufferGeometry(125, 64, 64);
+        var erthG = new THREE.SphereBufferGeometry(150, 64, 64);
+        var marsG = new THREE.SphereBufferGeometry(137.5, 64, 64);
+        var juptG = new THREE.SphereBufferGeometry(2500, 64, 64);
+        var strnG = new THREE.SphereBufferGeometry(750, 64, 64);
+        var urnsG = new THREE.SphereBufferGeometry(1000, 64, 64);
+        var neptG = new THREE.SphereBufferGeometry(600, 64, 64);
+
+        function getURLByQuality(base, quality) {
+            switch (quality) {
+                case "lowest":
+                    return base + "_512.png";
+                case "low":
+                    return base + "_1k.png";
+                case "medium":
+                    return base + "_2k.png";
+                case "high":
+                    return base + "_4k.png";
+                case "ultra":
+                    return base + "_8k.png";
+            }
+        }
+
+        var txtl = new THREE.TextureLoader();
+
+        log("Loading planet textures..")
+        camera.position.z = 5000;
+        window.camera = camera;
+
+        var sunT  = txtl.load(getURLByQuality("./assets/sun/sun", qualitySetting));
+        var mercT = txtl.load(getURLByQuality("./assets/merc/merc", qualitySetting));
+        var vensT = txtl.load(getURLByQuality("./assets/vens/vens", qualitySetting));
+        var erthT = txtl.load(getURLByQuality("./assets/erth/erth", qualitySetting));
+        var marsT = txtl.load(getURLByQuality("./assets/mars/mars", qualitySetting));
+        var juptT = txtl.load(getURLByQuality("./assets/jupt/jupt", qualitySetting));
+        var strnT = txtl.load(getURLByQuality("./assets/strn/strn", qualitySetting));
+        var urnsT = txtl.load(getURLByQuality("./assets/urns/urns", qualitySetting));
+        var neptT = txtl.load(getURLByQuality("./assets/nept/nept", qualitySetting));
+
+        var sunL  = new THREE.MeshBasicMaterial({map: sunT})
+        var mercL = new THREE.MeshLambertMaterial({map: mercT})
+        var vensL = new THREE.MeshLambertMaterial({map: vensT})
+        var erthL = new THREE.MeshLambertMaterial({map: erthT})
+        var marsL = new THREE.MeshLambertMaterial({map: marsT})
+        var juptL = new THREE.MeshLambertMaterial({map: juptT})
+        var strnL = new THREE.MeshLambertMaterial({map: strnT})
+        var urnsL = new THREE.MeshLambertMaterial({map: urnsT})
+        var neptL = new THREE.MeshLambertMaterial({map: neptT})
+        /*var sunL = new THREE.MeshBasicMaterial({
+            color: 0xff0000
+        })
+        var mercL = new THREE.MeshBasicMaterial({
+            color: 0x00ff00
+        })
+        var vensL = new THREE.MeshBasicMaterial({
+            color: 0x0000ff
+        })
+        var erthL = new THREE.MeshBasicMaterial({
+            color: 0xffff00
+        })
+        var marsL = new THREE.MeshBasicMaterial({
+            color: 0xff00ff
+        })
+        var juptL = new THREE.MeshBasicMaterial({
+            color: 0x00ffff
+        })
+        var strnL = new THREE.MeshBasicMaterial({
+            color: 0x000000
+        })
+        var urnsL = new THREE.MeshBasicMaterial({
+            color: 0xffffff
+        })
+        var neptL = new THREE.MeshBasicMaterial({
+            color: 0x333333
+        })*/
+
+        var sunM = new THREE.Mesh(sunG, sunL);
+        var mercM = new THREE.Mesh(mercG, mercL);
+        var vensM = new THREE.Mesh(vensG, vensL);
+        var erthM = new THREE.Mesh(erthG, erthL);
+        var marsM = new THREE.Mesh(marsG, marsL);
+        var juptM = new THREE.Mesh(juptG, juptL);
+        var strnM = new THREE.Mesh(strnG, strnL);
+        var urnsM = new THREE.Mesh(urnsG, urnsL);
+        var neptM = new THREE.Mesh(neptG, neptL);
+
+        planets.push(sunM);
+        planets.push(mercM);
+        planets.push(vensM);
+        planets.push(erthM);
+        planets.push(marsM);
+        planets.push(juptM);
+        planets.push(strnM);
+        planets.push(urnsM);
+        planets.push(neptM);
+
+        planets.forEach(e => {
+            scene.add(e);
+        })
+        ws.onmessage = function (e) {
+            var json = JSON.parse(e.data);
+            switch (json[0]) {
+                case 0:
+                    log("got message");
+
+                    break;
+                case 1:
+                    json.forEach((e, i) => {
+                        if (i == 0) return;
+                        try {
+                            planets[e.c].position.x = e.x;
+                            planets[e.c].position.z = e.z;
+                        } catch {
+
+                        }
+                    })
+
+                    break;
+            }
+        }
+        ws.onopen = function (e) {
+            ws.send("0");
+        }
+
+        function animate() {
+            requestAnimationFrame(animate);
+            renderer.render(scene, camera);
+        }
+        animate();
+        window.planets = planets;
+    }
+    everysecond() {
+
+        this.dataPool = 0;
+    }
+}
+var game = new player();
+window.game = game;
+game.init();
