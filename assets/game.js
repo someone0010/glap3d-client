8f420cd5192d910773b46f271b80ef4e2526fdb8
Goblin011 <44930310+someone0010@users.noreply.github.com>
2020-04-20 16:11:08 +0200

    Update game.js

--- assets/game.js
+++ assets/game.js
@@ -281,11 +281,11 @@ class player {
                                     if (e.n == myinstance) {
                                         camera.position.y = e.y + 2;
                                         
-                                        var euler = new THREE.Euler().setFromQuaternion(new THREE.Quaternion(e.qx, e.qy, e.qz, e.qw));
+                                        var euler = new THREE.Euler().setFromQuaternion(new THREE.Quaternion(e.qx, e.qy, e.qz, e.qw).normalize());
                                         console.log(f);
                                         
-                                        camera.position.x = -Math.sin(euler.y) * 10 + e.x;
-                                        camera.position.z = -Math.cos(euler.y) * 10 + e.z;
+                                        camera.position.x = Math.sin(euler.y) * 10 + e.x;
+                                        camera.position.z = Math.cos(euler.y) * 10 + e.z;
                                         camera.lookAt(f.position);
                                     }
                                 }
