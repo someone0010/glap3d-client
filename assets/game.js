d456f238343c8ad3ce42864b74d27ffe64d9bb5f
Goblin011 <44930310+someone0010@users.noreply.github.com>
2020-04-22 14:55:10 +0200

    Update game.js

--- assets/game.js
+++ assets/game.js
@@ -446,7 +446,7 @@ function startTime() {
         var zoomValue = 50;
         function animate() {
             requestAnimationFrame(animate);
-            camera.position.x = (Math.sin(euler.y) - Math.sin(euler.x)) * zoomValue + lastPlayerX;
+            camera.position.x = (Math.sin(euler.y) + Math.cos(euler.x)) * zoomValue + lastPlayerX;
             camera.position.z = (Math.cos(euler.y) - Math.sin(euler.x)) * zoomValue + lastPlayerZ;
             camera.position.y = -Math.sin(euler.x) * zoomValue + lastPlayerY;
             composer.render();
