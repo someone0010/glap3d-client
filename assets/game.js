b83418af5acdfc985c837877d5bde4b4014c5a3e
Goblin011 <44930310+someone0010@users.noreply.github.com>
2020-04-25 17:08:43 +0200

    Update game.js

--- assets/game.js
+++ assets/game.js
@@ -149,9 +149,11 @@ class player {
         function onWindowResize() {
 
             camera.aspect = window.innerWidth / window.innerHeight;
-            camera.updateProjectionMatrix();
+            
 
             renderer.setSize(window.innerWidth, window.innerHeight);
+            
+            camera.updateProjectionMatrix();
 
         }
         window.addEventListener('resize', onWindowResize, false);
